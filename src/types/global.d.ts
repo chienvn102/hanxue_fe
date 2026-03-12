2d91b062d79e52554147e222d3816167284668a6
chienvn102 <chienvn102@gmail.com>
2026-03-12 18:21:33 +0700

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,20 @@
-export { };
+interface YTPlayer {
+    getCurrentTime(): number;
+    getDuration(): number;
+    seekTo(seconds: number, allowSeekAhead: boolean): void;
+    playVideo(): void;
+    destroy(): void;
+}
 
-declare global {
-    interface Window {
-        YT: any;
-        onYouTubeIframeAPIReady: (() => void) | undefined;
-    }
+interface Window {
+    YT: {
+        Player: new (elementId: string, config: {
+            height?: string;
+            width?: string;
+            videoId?: string;
+            playerVars?: Record<string, number | string>;
+            events?: Record<string, (event: { data: number; target: YTPlayer }) => void>;
+        }) => YTPlayer;
+    };
+    onYouTubeIframeAPIReady: (() => void) | undefined;
 }
